e8f644eca2fcf4b6cffee2fca65e021356f86860
ProfEmmy <faithogbole4@gmail.com>
2023-06-19 14:57:58 -0700

    third commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const menu_icon = document.querySelector(".menu-icon")
 const mobile_nav = document.querySelector(".mobile-navlinks-div")
+const body = document.querySelector("body")
 
 menu_icon.addEventListener("click", () => {
     mobile_nav.classList.toggle("display")
+    body.classList.toggle("hide-scroll")
 })
